1393be8610ac5b1608d0b901d910ad5c45b7d514
Jacques Archimède <eureka@dbmail.com>
2016-08-20 19:41:39 +0200

    implementation of tv4 to validate JSON files

--- src/EUREKA/G6KBundle/Resources/public/admin/js/g6k.admin.datasources.js
+++ src/EUREKA/G6KBundle/Resources/public/admin/js/g6k.admin.datasources.js
@@ -622,7 +622,10 @@ THE SOFTWARE.
 		return errors;
 	}
 
-	Datasources.showErrors = function(errors) {
+	Datasources.showErrors = function(errors, message) {
+		if (message) {
+			$('.alert .error-message').text(message);
+		}
 		var mess = $('.alert ul');
 		mess.empty();
 		$.each(errors, function( index, value ) {
@@ -632,6 +635,7 @@ THE SOFTWARE.
 	}
 
 	Datasources.hideErrors = function() {
+		$('.alert .error-message').empty();
 		$('.alert ul').empty();
 		$('.alert').hide();
 	}
@@ -708,6 +712,9 @@ $(document).ready(function() {
 			});
 		}
 		if ( $("#datasource-import-form" ).length) {
+			if (Admin.lang == 'de' || Admin.lang == 'es' || Admin.lang == 'fr') { 
+				tv4.language(Admin.lang);
+			}
 			$("#datasource-import-form input[name='datasource-schema-file'], #datasource-import-form input[name='datasource-data-file']").change(function (e) {
 			Datasources.hideErrors();
 			var files = e.target.files;
@@ -726,6 +733,7 @@ $(document).ready(function() {
 				var name = '';
 				var schemainput = $("#datasource-import-form input[name='datasource-schema-file']");
 				var schemafile = schemainput.val();
+				messageheader = '';
 				if (schemafile == '') {
 					errors.push("Please, choose a JSON schema file");
 				} else if (! /\.schema\.json$/.test(schemafile)) {
@@ -745,25 +753,47 @@ $(document).ready(function() {
 					if (name != m[1]) {
 						errors.push("The names of the two files without extension should be the same.");
 					} else {
-						var schema = schemainput.data('content');
-						var data = datainput.data('content');
+						var metaschema = JSON.parse('{"id": "http://json-schema.org/draft-04/schema#", "$schema": "http://json-schema.org/draft-04/schema#", "description": "Core schema meta-schema", "definitions": {"schemaArray": {"type": "array", "minItems": 1, "items": {"$ref": "#"}}, "positiveInteger": {"type": "integer", "minimum": 0}, "positiveIntegerDefault0": {"allOf": [{"$ref": "#/definitions/positiveInteger"}, {"default": 0}]}, "simpleTypes": {"enum": ["array", "boolean", "integer", "null", "number", "object", "string"]}, "stringArray": {"type": "array", "items": {"type": "string"}, "minItems": 1, "uniqueItems": true}}, "type": "object", "properties": {"id": {"type": "string", "format": "uri"}, "$schema": {"type": "string", "format": "uri"}, "title": {"type": "string"}, "description": {"type": "string"}, "default": {}, "multipleOf": {"type": "number", "minimum": 0, "exclusiveMinimum": true}, "maximum": {"type": "number"}, "exclusiveMaximum": {"type": "boolean", "default": false}, "minimum": {"type": "number"}, "exclusiveMinimum": {"type": "boolean", "default": false}, "maxLength": {"$ref": "#/definitions/positiveInteger"}, "minLength": {"$ref": "#/definitions/positiveIntegerDefault0"}, "pattern": {"type": "string", "format": "regex"}, "additionalItems": {"anyOf": [{"type": "boolean"}, {"$ref": "#"}], "default": {}}, "items": {"anyOf": [{"$ref": "#"}, {"$ref": "#/definitions/schemaArray"}], "default": {}}, "maxItems": {"$ref": "#/definitions/positiveInteger"}, "minItems": {"$ref": "#/definitions/positiveIntegerDefault0"}, "uniqueItems": {"type": "boolean", "default": false}, "maxProperties": {"$ref": "#/definitions/positiveInteger"}, "minProperties": {"$ref": "#/definitions/positiveIntegerDefault0"}, "required": {"$ref": "#/definitions/stringArray"}, "additionalProperties": {"anyOf": [{"type": "boolean"}, {"$ref": "#"}], "default": {}}, "definitions": {"type": "object", "additionalProperties": {"$ref": "#"}, "default": {}}, "properties": {"type": "object", "additionalProperties": {"$ref": "#"}, "default": {}}, "patternProperties": {"type": "object", "additionalProperties": {"$ref": "#"}, "default": {}}, "dependencies": {"type": "object", "additionalProperties": {"anyOf": [{"$ref": "#"}, {"$ref": "#/definitions/stringArray"}]}}, "enum": {"type": "array", "minItems": 1, "uniqueItems": true}, "type": {"anyOf": [{"$ref": "#/definitions/simpleTypes"}, {"type": "array", "items": {"$ref": "#/definitions/simpleTypes"}, "minItems": 1, "uniqueItems": true}]}, "allOf": {"$ref": "#/definitions/schemaArray"}, "anyOf": {"$ref": "#/definitions/schemaArray"}, "oneOf": {"$ref": "#/definitions/schemaArray"}, "not": {"$ref": "#"}}, "dependencies": {"exclusiveMaximum": ["maximum"], "exclusiveMinimum": ["minimum"]}, "default": {}}');
+						var schema = JSON.parse(schemainput.data('content'));
+						var data = JSON.parse(datainput.data('content'));
 
-						var valid = tv4.validate(data, schema, true, true);
-						if (!valid) {
-							errors.push("Validation error : " + tv4.error.message); 
+						var result = tv4.validateMultiple(data, schema, true, true);
+						if (!result.valid) {
+							messageheader = 'Validation errors : ';
+							$.each(result.errors, function (e, error) {
+								var dataPath = "";
+								if (error.dataPath) {
+									var table = "";
+									var row = 0;
+									var paths = error.dataPath.split("/");
+									var len = paths.length
+									$.each(paths, function (p, path) {
+										switch (p) {
+											case 1:
+												table = path;
+												dataPath += "Table '" + table + "'";
+												break;
+											case 2:
+												row = parseInt(path);
+												dataPath += "Row " + (row + 1);
+												break;
+											case 3:
+												dataPath += "Field '" + path + "', ";
+												dataPath += "Data '" + data[table][row][path] + "'";
+												break;
+										}
+										dataPath += p == 0 || p == len - 1 ? '' : ', ';
+									});
+									dataPath += " : ";
+								}
+								errors.push(dataPath + error.message);
+							});
 						}
-						
-						// var ajv = new Ajv({ useDefaults: true });
-						// var valid = ajv.validate(schema, data);
-						// if (!valid) {
-							// errors.push("Validation error : " + ajv.errors); 
-						// }
-
 					}
 				}
 				if (errors.length > 0) {
 					e.preventDefault();
-					Datasources.showErrors(errors);
+					Datasources.showErrors(errors, messageheader);
 					return false;
 				}
 				Datasources.hideErrors();
