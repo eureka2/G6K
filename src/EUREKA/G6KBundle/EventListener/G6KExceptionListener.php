87fa665d0047f0e3c49d9f9c524c2864f31c9617
Jacques Archimède <eureka@dbmail.com>
2017-07-18 23:06:49 +0200

    Enhancement of the exception listener

--- src/EUREKA/G6KBundle/EventListener/G6KExceptionListener.php
+++ src/EUREKA/G6KBundle/EventListener/G6KExceptionListener.php
@@ -2,36 +2,56 @@
 
 namespace EUREKA\G6KBundle\EventListener;
 
+
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class G6KExceptionListener
 {
-    public function onKernelException(GetResponseForExceptionEvent $event)
-    {
-        // You get the exception object from the received event
-        $exception = $event->getException();
-        $message = sprintf(
-            'Error: %s with code: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
+	protected $container;
 
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent($message);
+	public function __construct(ContainerInterface $container) {
+		$this->container = $container;
+	}
 
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+	public function onKernelException(GetResponseForExceptionEvent $event) {
+		$exception = $event->getException();
+		$request = $event->getRequest();
+		$domain = $request->getHost();
+		$domainview = $this->container->getParameter('domainview');
+		$view = "Default";
+		foreach ($domainview as $d => $v) {
+			if (preg_match("/".$d."$/", $domain)) {
+				$view = $v;
+				break;
+			}
+		}
+		$loader = new \Twig_Loader_Array(array(
+			'error.html.twig' => 'Error : {{ message }} with code : {{ code }}',
+		));
+		$twig = new \Twig_Environment($loader);
 
-        // Send the modified response object to the event
-        $event->setResponse($response);
-    }
+		$response = new Response();
+		$response->setContent(
+			$twig->render(
+				'error.html.twig', 
+				array(
+					'message' => $exception->getMessage(),
+					'code' => $exception->getCode()
+				)
+			)
+		);
+
+		if ($exception instanceof HttpExceptionInterface) {
+			$response->setStatusCode($exception->getStatusCode());
+			$response->headers->replace($exception->getHeaders());
+		} else {
+			$response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+		}
+
+		// Send the modified response object to the event
+		$event->setResponse($response);
+	}
 }
