1bb12309e86c58af5428af13e860bb1a01ebbb7c
Jacques Archimède <eureka@dbmail.com>
2017-10-21 22:40:33 +0200

    Updated classes documentation

--- src/EUREKA/G6KBundle/Entity/Panel.php
+++ src/EUREKA/G6KBundle/Entity/Panel.php
@@ -3,7 +3,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2015 Jacques Archimède
+Copyright (c) 2015-2017 Jacques Archimède
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,64 +26,219 @@ THE SOFTWARE.
 
 namespace EUREKA\G6KBundle\Entity;
 
+/**
+ * This class allows the storage and retrieval of the attributes of a panel.
+ *
+ * A panel is where the informations of a simulation step are displayed.
+ *
+ * Information can be organized in one or more panels.
+ *
+ * If there are multiple panels, they are displayed as tabs.
+ *
+ * The first levels of information in a panel are field sets or blocks of information.
+ *
+ * @author    Jacques Archimède
+ *
+ */
 class Panel {
 
+	/**
+	 * @var \EUREKA\G6KBundle\Entity\Step $step The Step object that contains this panel.  
+	 *
+	 * @access  private
+	 *
+	 */
 	private $step = null;
+
+	/**
+	 * @var int        $id The id of this panel.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $id = 0;
+
+	/**
+	 * @var string     $name The name of this panel.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $name = "";
+
+	/**
+	 * @var string     $label The label of this panel.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $label = "";
+
+	/**
+	 * @var array      $fieldsets The list of field sets (FieldSet objects) or block of informations (BlockInfo objects) contained in this panel
+	 *
+	 * @access  private
+	 *
+	 */
 	private $fieldsets = array();
+
+	/**
+	 * @var bool       $displayable Indicates whether this panel should be displayed or not.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $displayable = true;
 
+	/**
+	 * Constructor of class Panel
+	 *
+	 * @access  public
+	 * @param   \EUREKA\G6KBundle\Entity\Step $step The Step object that contains this panel.   
+	 * @param   int        $id The id of the panel.
+	 * @return  void
+	 *
+	 */
 	public function __construct($step, $id) {
 		$this->step = $step;
 		$this->id = $id;
 	}
 
+	/**
+	 * Returns the Step object that contains this panel
+	 *
+	 * @access  public
+	 * @return  \EUREKA\G6KBundle\Entity\Step The Step object
+	 *
+	 */
 	public function getStep() {
 		return $this->step;
 	}
 
+	/**
+	 * Returns the id of this panel
+	 *
+	 * @access  public
+	 * @return  int The id of this panel
+	 *
+	 */
 	public function getId() {
 		return $this->id;
 	}
 
+	/**
+	 * Sets the id of this panel
+	 *
+	 * @access  public
+	 * @param   int        $id The id of this panel
+	 * @return  void
+	 *
+	 */
 	public function setId($id) {
 		$this->id = $id;
 	}
 
+	/**
+	 * Returns the name of this panel
+	 *
+	 * @access  public
+	 * @return  string The name of this panel
+	 *
+	 */
 	public function getName() {
 		return $this->name;
 	}
 
+	/**
+	 * Sets the name of this panel
+	 *
+	 * @access  public
+	 * @param   string     $name The name of this panel
+	 * @return  void
+	 *
+	 */
 	public function setName($name) {
 		$this->name = $name;
 	}
 
+	/**
+	 * Returns the label of this panel
+	 *
+	 * @access  public
+	 * @return  string The label of this panel
+	 *
+	 */
 	public function getLabel() {
 		return $this->label;
 	}
 
+	/**
+	 * Sets the label of this panel
+	 *
+	 * @access  public
+	 * @param   string     $label The label of this panel
+	 * @return  void
+	 *
+	 */
 	public function setLabel($label) {
 		$this->label = $label;
 	}
 
+	/**
+	 * Returns the list of field sets (FieldSet objects) or block of informations (BlockInfo objects) contained in this panel.
+	 *
+	 * @access  public
+	 * @return  array The list of FieldSet or BlockInfo objects
+	 *
+	 */
 	public function getFieldSets() {
 		return $this->fieldsets;
 	}
 
+	/**
+	 * Sets the list of field sets (FieldSet objects) or block of informations (BlockInfo objects) contained in this panel.
+	 *
+	 * @access  public
+	 * @param   array $fieldsets The list of FieldSet or BlockInfo objects
+	 * @return  void
+	 *
+	 */
 	public function setFieldSets($fieldsets) {
 		$this->fieldsets = $fieldsets;
 	}
 
+	/**
+	 * Adds a Fieldset or BlockInfo object to the list of field sets or block of informations contained in this panel.
+	 *
+	 * @access  public
+	 * @param   \EUREKA\G6KBundle\Entity\Fieldset|\EUREKA\G6KBundle\Entity\BlockInfo $fieldset The FieldSet or BlockInfo object
+	 * @return  void
+	 *
+	 */
 	public function addFieldSet($fieldset) {
 		$this->fieldsets[] = $fieldset;
 	}
 
+	/**
+	 * Removes a Fieldset or BlockInfo object from the list of field sets or block of informations contained in this panel.
+	 *
+	 * @access  public
+	 * @param   int $index The index of the Fieldset or BlockInfo object in the list
+	 * @return  void
+	 *
+	 */
 	public function removeFieldSet($index) {
 		$this->fieldsets[$index] = null;
 	}
 
+	/**
+	 * Retrieves a Fieldset object by its id in the list of field sets contained in this panel.
+	 *
+	 * @access  public
+	 * @param   int $id The id of Fieldset object 
+	 * @return  \EUREKA\G6KBundle\Entity\Fieldset|null The Fieldset object
+	 *
+	 */
 	public function getFieldSetById($id) {
 		foreach ($this->fieldsets as $fieldset) {
 			if ($fieldset instanceof FieldSet && $fieldset->getId() == $id) {
@@ -93,6 +248,14 @@ class Panel {
 		return null;
 	}
 
+	/**
+	 * Retrieves a BlockInfo object by its id in the list of blocks of informations contained in this panel.
+	 *
+	 * @access  public
+	 * @param   int $id The id of the BlockInfo object
+	 * @return  \EUREKA\G6KBundle\Entity\BlockInfo|null The BlockInfo object
+	 *
+	 */
 	public function getBlockInfoById($id) {
 		foreach ($this->fieldsets as $fieldset) {
 			if ($fieldset instanceof BlockInfo && $fieldset->getId() == $id) {
@@ -102,6 +265,15 @@ class Panel {
 		return null;
 	}
 
+	/**
+	 * Searchs if there is a block of information of which at least one of the chapters can be folded / unfolded and returns true or false according to the result of the search.
+	 *
+	 * This method is an alias of getCollapsibles.
+	 *
+	 * @access  public
+	 * @return  bool true if a collapsible chapter exists, false otherwise
+	 *
+	 */
 	public function hasCollapsibles() {
 		foreach ($this->fieldsets as $block) {
 			if ($block instanceof BlockInfo) {
@@ -115,18 +287,47 @@ class Panel {
 		return false;
 	}
 
+	/**
+	 * Searchs if there is a block of information of which at least one of the chapters can be folded / unfolded and returns true or false according to the result of the search.
+	 *
+	 * @access  public
+	 * @return  bool true if a collapsible chapter exists, false otherwise
+	 *
+	 */
 	public function getCollapsibles() {
 		return $this->hasCollapsibles();
 	}
 
+	/**
+	 * Returns the displayable attribute of this Panel object 
+	 *
+	 * @access  public
+	 * @return  bool  true if this panel can be displayed, false otherwise
+	 *
+	 */
 	public function isDisplayable() {
 		return $this->displayable;
 	}
 
+	/**
+	 * Returns the displayable attribute of this Panel object 
+	 *
+	 * @access  public
+	 * @return  bool  true if this panel can be displayed, false otherwise
+	 *
+	 */
 	public function getDisplayable() {
 		return $this->displayable;
 	}
 
+	/**
+	 * Determines whether this panel can be displayed or not
+	 *
+	 * @access  public
+	 * @param   bool       $displayable true if this panel can be displayed, false otherwise
+	 * @return  void
+	 *
+	 */
 	public function setDisplayable($displayable) {
 		$this->displayable = $displayable;
 	}
--- src/EUREKA/G6KBundle/Entity/Parameter.php
+++ src/EUREKA/G6KBundle/Entity/Parameter.php
@@ -3,7 +3,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2015 Jacques Archimède
+Copyright (c) 2015-2017 Jacques Archimède
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,84 +26,312 @@ THE SOFTWARE.
 
 namespace EUREKA\G6KBundle\Entity;
 
+/**
+ * This class allows the storage and retrieval of the attributes of a parameter.
+ *
+ * A parameter is the way to pass on values of data or constants either to parameterized queries upon a data source or as the parameter of the web service url of a data source.
+ * Therefore, a parameter is always associated with a source.
+ *
+ * @author    Jacques Archimède
+ *
+ */
 class Parameter {
 
+	/**
+	 * @var \EUREKA\G6KBundle\Entity\Source $source The Source object that uses this parameter.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $source = null;
+
+	/**
+	 * @var string     $type The type of this parameter: 'queryString', 'path', 'data', 'columnValue' or 'header'
+	 *
+	 * @access  private
+	 *
+	 */
 	private $type = "";
-	private $origin = "data"; // 'data' or 'constant'
+
+	/**
+	 * @var string     $origin The origin of this parameter: 'data' or 'constant'
+	 *
+	 * @access  private
+	 *
+	 */
+	private $origin = "data"; 
+
+	/**
+	 * @var string     $name The name of this parameter
+	 *
+	 * @access  private
+	 *
+	 */
 	private $name = "";
+
+	/**
+	 * @var string     $format  The format in which the parameter must be provided. Useful for a parameter of type date.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $format = "";
+
+	/**
+	 * @var int        $data  The id of the data item associated with this parameter if the origin attribute is equal to 'data'.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $data = 0;
+
+	/**
+	 * @var string     $constant The constant value of this parameter if the origin attribute is equal to 'constant'.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $constant = "";
+
+	/**
+	 * @var bool       $optional  Indicates whether this parameter is optional or not.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $optional = false;
 
+	/**
+	 * Constructor of class Parameter
+	 *
+	 * @access  public
+	 * @param   \EUREKA\G6KBundle\Entity\Source $source The Source object that uses this parameter.
+	 * @param   string     $type The type of this parameter
+	 * @return  void
+	 *
+	 */
 	public function __construct($source, $type) {
 		$this->source = $source;
 		$this->type = $type;
 	}
 
+	/**
+	 * Returns the Source object that uses this parameter.
+	 *
+	 * @access  public
+	 * @return  \EUREKA\G6KBundle\Entity\Source The Source object 
+	 *
+	 */
 	public function getSource() {
 		return $this->source;
 	}
 
+	/**
+	 * Returns the type of this parameter.
+	 *
+	 * The possible values are:
+	 *
+	 * - queryString: The parameter is part of the queryString of the url (if the source is a web service with method "GET")
+	 * - path: the parameter is part of the path of the url (if the source is a web service)
+	 * - data: the parameter is a POST data (if the source is a web service with method "POST")
+	 * - columnValue: the parameter is in the where clause of an SQL query (if the source is an internal or external database).
+	 * - header: the parameter is a HTTP header (if the source is a web service).
+	 *
+	 * @access  public
+	 * @return  string The type of this parameter
+	 *
+	 */
 	public function getType() {
 		return $this->type;
 	}
 
+	/**
+	 * Sets the type of this parameter.
+	 *
+	 * The possible values are:
+	 *
+	 * - queryString: The parameter is part of the queryString of the url (if the source is a web service with method "GET")
+	 * - path: the parameter is part of the path of the url (if the source is a web service)
+	 * - data: the parameter is a POST data (if the source is a web service with method "POST")
+	 * - columnValue: the parameter is in the where clause of a SQL request (if the source is an internal or external database).
+	 * - header: the parameter is a HTTP header (if the source is a web service).
+	 *
+	 * @access  public
+	 * @param   string     $type The type of this parameter
+	 * @return  void
+	 *
+	 */
 	public function setType($type) {
 		$this->type = $type;
 	}
 
+	/**
+	 * Returns the origin of this parameter
+	 *
+	 * The possible values are:
+	 *
+	 * - data: the content of this parameter is the value of the data item associated with it.
+	 * - constant: the content of this parameter is a constant value. 
+	 *
+	 * @access  public
+	 * @return  string The origin of this parameter
+	 *
+	 */
 	public function getOrigin() {
 		return $this->origin;
 	}
 
+	/**
+	 * Sets the origin of this parameter
+	 *
+	 * The possible values are:
+	 *
+	 * - data: the content of this parameter is the value of the data item associated with it.
+	 * - constant: the content of this parameter is a constant value. 
+	 *
+	 * @access  public
+	 * @param   string     $origin The origin of this parameter
+	 * @return  void
+	 *
+	 */
 	public function setOrigin($origin) {
 		if ($origin != '') {
 			$this->origin = $origin;
 		}
 	}
 
+	/**
+	 * Returns the name of this parameter
+	 *
+	 * @access  public
+	 * @return  string The name of this parameter
+	 *
+	 */
 	public function getName() {
 		return $this->name;
 	}
 
+	/**
+	 * Sets the name of this parameter
+	 *
+	 * @access  public
+	 * @param   string     $name The name of this parameter
+	 * @return  void
+	 *
+	 */
 	public function setName($name) {
 		$this->name = $name;
 	}
 
+	/**
+	 * Returns the format in which the parameter must be provided. Useful only for a parameter of type date.
+	 *
+	 * @see http://php.net/manual/fr/function.date.php to know the characters used in a format.
+	 *
+	 * @access  public
+	 * @return  string The format in which the parameter must be provided.
+	 *
+	 */
 	public function getFormat() {
 		return $this->format;
 	}
 
+	/**
+	 * Sets the format in which the parameter must be provided. Useful only for a parameter of type date.
+	 *
+	 * @see http://php.net/manual/fr/function.date.php to know the characters used in a format.
+	 *
+	 * @access  public
+	 * @param   string     $format  The format in which the parameter must be provided.
+	 * @return  void
+	 *
+	 */
 	public function setFormat($format) {
 		$this->format = $format;
 	}
 
+	/**
+	 * Returns the id of the data item associated with this parameter. Useful only if origin is equal to 'data'.
+	 *
+	 * @access  public
+	 * @return  int The id of the data item associated with this parameter.
+	 *
+	 */
 	public function getData() {
 		return $this->data;
 	}
 
+	/**
+	 * Sets the id of the data item associated with this parameter. Useful only if origin is equal to 'data'.
+	 *
+	 * @access  public
+	 * @param   int        $data The id of the data item associated with this parameter.
+	 * @return  void
+	 *
+	 */
 	public function setData($data) {
 		$this->data = $data;
 	}
 
+	/**
+	 * Returns the constant value of this parameter. Useful only if origin is equal to 'constant'.
+	 *
+	 * @access  public
+	 * @return  string The constant value of this parameter.
+	 *
+	 */
 	public function getConstant() {
 		return $this->constant;
 	}
 
+	/**
+	 * Sets the constant value of this parameter. Useful only if origin is equal to 'constant'.
+	 *
+	 * @access  public
+	 * @param   string     $constant The constant value of this parameter.
+	 * @return  void
+	 *
+	 */
 	public function setConstant($constant) {
 		$this->constant = $constant;
 	}
 
+	/**
+	 * Returns the optional attribute of this parameter.
+	 *
+	 * An optional parameter with no value is not passed to the parameterized query or not inserted in the url.
+	 *
+	 * @access  public
+	 * @return  bool true if this parameter is optional, false otherwise
+	 *
+	 */
 	public function isOptional() {
 		return $this->optional;
 	}
 
+	/**
+	 * Returns the optional attribute of this parameter.
+	 *
+	 * An optional parameter with no value is not passed to the parameterized query or not inserted in the url.
+	 *
+	 * @access  public
+	 * @return  bool true if this parameter is optional, false otherwise
+	 *
+	 */
 	public function getOptional() {
 		return $this->optional;
 	}
 
+	/**
+	 * Determines whether this parameter is optional or not.
+	 *
+	 * An optional parameter with no value is not passed to the parameterized query or not inserted in the url.
+	 *
+	 * @access  public
+	 * @param   bool $optional true if this parameter is optional, false otherwise
+	 * @return  void
+	 *
+	 */
 	public function setOptional($optional) {
 		$this->optional = $optional;
 	}
