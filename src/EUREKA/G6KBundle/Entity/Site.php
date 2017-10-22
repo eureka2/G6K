e6b2150965094ca20bcaabd185ae52f39100389f
Jacques Archimède <eureka@dbmail.com>
2017-10-22 22:04:06 +0200

    Updated classes documentation

--- src/EUREKA/G6KBundle/Entity/Site.php
+++ src/EUREKA/G6KBundle/Entity/Site.php
@@ -26,44 +26,140 @@ THE SOFTWARE.
 
 namespace EUREKA\G6KBundle\Entity;
 
+/**
+ * This class allows the storage and retrieval of the attributes of a web site in which a simulator is ran.
+ *
+ * @author    Jacques Archimède
+ *
+ */
 class Site {
 
+	/**
+	 * @var \EUREKA\G6KBundle\Entity\Simulator $simulator The Simulator object that uses this web site.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $simulator = null;
+
+	/**
+	 * @var int      $id The id of this Site object.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $id;
+
+	/**
+	 * @var string      $name The name of this web site.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $name;
+
+	/**
+	 * @var string      $home The URL of the home page of this web site.
+	 *
+	 * @access  private
+	 *
+	 */
 	private $home;
-	
+
+	/**
+	 * Constructor of class Site
+	 *
+	 * @access  public
+	 * @param   \EUREKA\G6KBundle\Entity\Simulator $simulator The Simulator object that uses this web site.
+	 * @param   string $id The id of this Site object.
+	 * @param   string $name The name of this web site.
+	 * @param   string $home The URL of the home page of this web site.
+	 * @return  void
+	 *
+	 */
 	public function __construct($simulator, $id, $name, $home) {
 		$this->simulator = $simulator;
 		$this->id = $id;
 		$this->name = $name;
 		$this->home = $home;
 	}
-	
+
+	/**
+	 * Returns the Simulator object that uses this web site.
+	 *
+	 * @access  public
+	 * @return  \EUREKA\G6KBundle\Entity\Simulator The Simulator object.
+	 *
+	 */
 	public function getSimulator() {
 		return $this->simulator;
 	}
-	
+
+	/**
+	 * Returns the id of this Site object
+	 *
+	 * @access  public
+	 * @return  int The id of this Site object
+	 *
+	 */
 	public function getId() {
 		return $this->id;
 	}
-	
+
+	/**
+	 * Sets the id of this Site object
+	 *
+	 * @access  public
+	 * @param   int $id The id of this Site object
+	 * @return  void
+	 *
+	 */
 	public function setId($id) {
 		$this->id = $id;
 	}
-	
+
+	/**
+	 * Returns the name of this web site.
+	 *
+	 * @access  public
+	 * @return  string The name of this web site.
+	 *
+	 */
 	public function getName() {
 		return $this->name;
 	}
-	
+
+	/**
+	 * Sets the name of this web site.
+	 *
+	 * @access  public
+	 * @param   string $name The name of this web site.
+	 * @return  void
+	 *
+	 */
 	public function setName($name) {
 		$this->name = $name;
 	}
-	
+
+	/**
+	 * Returns the URL of the home page of this web site.
+	 *
+	 * @access  public
+	 * @return  string The URL of the home page
+	 *
+	 */
 	public function getHome() {
 		return $this->home;
 	}
-	
+
+	/**
+	 * Sets the URL of the home page of this web site.
+	 *
+	 * @access  public
+	 * @param   string $home The URL of the home page
+	 * @return  void
+	 *
+	 */
 	public function setHome($home) {
 		$this->home = $home;
 	}
