9462f5374dfec53e3bc95b1f99986eaed9a94c1a
Jacques Archimède <eureka@dbmail.com>
2019-01-17 22:53:58 +0100

    Added english and italian languages in the expression builder

---
+++ calcul/assets/admin/js/libs/jquery.expressionbuilder/lang/it.js
@@ -0,0 +1,45 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2019 Jacques Archimède
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+ExpressionBuilder_I18N['it'] = {
+		'menu-action-change': 'Modificare questa operando',
+		'menu-action-add': 'Aggiungi operando dopo',
+		'menu-action-insert': 'Inserire operando prima',
+		'menu-action-nested': 'Passa all\'espressione nidificata',
+		'menu-action-delete': 'Cancella questo operando',
+		'menu-action-add-argument':  'Aggiungi un argomento a questa funzione',
+		'literal-label': 'Letterale',
+		'literal-placeholder': 'Inserire un valore',
+		'fields-label': 'Campi',
+		'constants-label': 'Costanti',
+		'functions-label': 'Funzioni',
+		'parameters-label': 'Parametri',
+		'miscellaneous-label': 'Varie',
+		'nested-expression-label': 'Espressione annidata',
+		'operand-holder-tip': 'Clicca per modificare questo operando oppure fare clic destro per un\'azione più',
+		'operator-holder-tip': 'Clicca per cambiare questo operatore',
+		'nested-expression-delete-tip': 'Clicca per cambiare questo questo termine nidificato',
+		'missing-left-parenthesis': 'Manca parentesi sinistra',
+		'missing-right-parenthesis': 'Manca parentesi destra'
+}
