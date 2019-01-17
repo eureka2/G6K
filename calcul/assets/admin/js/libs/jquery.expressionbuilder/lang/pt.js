24cec710b0cb33f67230e6c5f99a66dbaa010b2b
Jacques Archimède <eureka@dbmail.com>
2019-01-17 23:27:00 +0100

    Addition of the Portuguese translation

---
+++ calcul/assets/admin/js/libs/jquery.expressionbuilder/lang/pt.js
@@ -0,0 +1,45 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Jacques Archimède
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+ExpressionBuilder_I18N['pt'] = {
+	'menu-action-change': 'Alterar este operando',
+	'menu-action-add': 'Adicionar um operando após este',
+	'menu-action-insert': 'Inserir um operando antes deste',
+	'menu-action-nested': 'Alterar para uma subexpressão entre parênteses&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+	'menu-action-delete': 'Excluir este operando',
+	'menu-action-add-argument': 'Adicione um argumento a esta função',
+	'literal-label': 'Constante',
+	'literal-placeholder': 'Insira um valor',
+	'fields-label': 'Campos',
+	'constants-label': 'Constantes',
+	'functions-label': 'Funções',
+	'parameters-label': 'Parâmetros',
+	'miscellaneous-label': 'Vário',
+	'nested-expression-label': 'Subexpressão',
+	'operand-holder-tip': 'Clique para alterar este operando ou clique com o botão direito do mouse para obter mais ações',
+	'operator-holder-tip': 'Clique para mudar este operador',
+	'nested-expression-delete-tip': 'Clique para alterar esta subexpressão',
+	'missing-left-parenthesis': 'Falta um parêntese de abertura',
+	'missing-right-parenthesis': 'Falta um parêntese de fechamento'
+}
