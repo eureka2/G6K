fa43f9b2eb472e485f67bc5adc763fbd8f77c2fa
Jacques Archimède <eureka@dbmail.com>
2019-04-25 20:56:09 +0200

    Improved AutoDateFormat widget

--- calcul/assets/base/widgets/AutoDateFormat/js/AutoDateFormat.js
+++ calcul/assets/base/widgets/AutoDateFormat/js/AutoDateFormat.js
@@ -3,6 +3,9 @@
 
 	// https://webdesign.tutsplus.com/tutorials/auto-formatting-input-value--cms-26745
 	function AutoDateFormat (input, options, onComplete) {
+		var numericKeys = [48,49,50,51,52,53,54,55,56,57];
+		var numericPadKeys = [96,97,98,99,100,101,102,103,104,105];
+		var controlKeys = [8, 9, 13, 35, 36, 37, 39, 46];
 		var date = input[0];
 		date.type = 'text';
 		var d = 0, m = 0, Y = 0;
@@ -35,21 +38,10 @@
 		}
 		partial = new RegExp(partial.replace('()?', ''));
 		complete = new RegExp(complete);
+		var allowedKeys = numericKeys.concat(numericPadKeys, controlKeys);
 
 		date.addEventListener('input', function(e) {
-			var values = splitValue(this.value);
-			if (values[m]) values[m] = padValue(values[m], 12);
-			if (values[d]) values[d] = padValue(values[d], 31);
-			var output = values.map(function(v, i) {
-				if (i == Y && v.length == 4) {
-					return v + sep[i];
-				} else if ((i == m || i == d) && v.length == 2) {
-					return v + sep[i];
-				} else {
-					return v;
-				}
-			});
-			this.value = output.join('').substr(0, maxlen);
+			this.value = fixValue(this.value);
 			if (complete.test(this.value)) {
 				onComplete(this.value, this.value);
 			}
@@ -58,20 +50,40 @@
 		date.addEventListener('keydown', function(e) {
 			// keydown is triggered before input
 			var key = e.which || e.keyCode;
-			if (key == 8 && date.value.length > 0) { // backspace
-				var value = date.value;
-				var last = value.slice(-1);
-				if (!/\d/.test(value.slice(-1))) {
+			var value = this.value;
+			var start = this.selectionStart;
+			if (key == 8 && this.selectionStart > 0) { // backspace
+				var toDelete = value.charAt(start - 1);
+				if (sep.indexOf(toDelete) >= 0) {
 					e.preventDefault();
-					value = value.slice(0, -1);
+					this.setSelectionRange(start - 1, start - 1);
+				} else {
+					e.preventDefault();
+					value = value.slice(0, start - 1) + ' ' + value.slice(start);
 					while (value.length > 0 && !/\d/.test(value.slice(-1))) {
 						value = value.slice(0, -1);
 					}
-					if (value.length > 0) {
-						value = value.slice(0, -1);
-					}
-					date.value = value;
+					this.value = value;
+					this.setSelectionRange(start - 1, start - 1);
 				}
+			} else if (value.charAt(start) == ' ' && 
+				(numericKeys.indexOf(key) >= 0 || numericPadKeys.indexOf(key) >= 0)) {
+				e.preventDefault();
+				e.stopPropagation();
+				var chr = numericPadKeys.indexOf(key) >= 0 ?
+						String.fromCharCode(key - 48) :
+						String.fromCharCode(key); 
+				value = value.slice(0, start) + chr + value.slice(start + 1);
+				var fixed = fixValue(value);
+				if (fixed.slice(0, start + 1) == value.slice(0, start + 1)) {
+					this.value = value;
+					this.setSelectionRange(start + 1, start + 1);
+					if (complete.test(this.value)) {
+						onComplete(this.value, this.value);
+					}
+				}
+			} else if (allowedKeys.indexOf(key) < 0 && sep.indexOf(e.key) < 0) {
+				e.preventDefault();
 			}
 		});
 
@@ -102,6 +114,22 @@
 				}
 			}
 		});
+
+		function fixValue(value) {
+			var values = splitValue(value);
+			if (values[m]) values[m] = padValue(values[m], 12);
+			if (values[d]) values[d] = padValue(values[d], 31);
+			var output = values.map(function(v, i) {
+				if (i == Y && v.length == 4) {
+					return v + sep[i];
+				} else if ((i == m || i == d) && v.length == 2) {
+					return v + sep[i];
+				} else {
+					return v;
+				}
+			});
+			return output.join('').substr(0, maxlen);
+		}
 
 		function splitValue(value) {
 			return value.match(partial).filter(function(v, i) {
